b0cad5e38244226ef1e63862d6730680aa402e7a
Natan Figueiredo <natanfigueiredo27@gmail.com>
2022-09-11 19:19:04 -0300

    initial commit

---
+++ src/main/java/br/com/elo/challenge/planetexplorer/PlanetExplorerApplication.java
@@ -0,0 +1,13 @@
+package br.com.elo.challenge.planetexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elo/challenge/planetexplorer/PlanetExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elo.challenge.planetexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
